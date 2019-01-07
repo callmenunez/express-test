468cf909d482106ee648fa3904bb7625e111290e
David Nunez <david.nunez@yodle.com>
2019-01-07 16:29:00 -0500

    updated routes, however may need to delete repo as localhost is not launching

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
+
 const express = require('express');
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
-
 
 const app = express();
 
@@ -10,49 +10,20 @@ app.use(cookieParser());
 
 app.set('view engine', 'pug');
 
-app.use((req, res, next) => {
-  console.log("Hello")
-  const err = new Error('SHIT!');
-  err.status = 500;
-  next();
-});
+const routes = require('./routes');
+app.use(routes);
 
-app.use((req, res, next) => {
-  console.log("world");
-  next();
-});
+// app.use((req, res, next) => {
+//   console.log("Hello")
+//   const err = new Error('SHIT!');
+//   err.status = 500;
+//   next();
+// });
 
-app.get('/', (req, res) => {
-  const name = req.cookies.username
-  if (name) {
-    res.render('index', { name });
-  } else {
-    res.redirect('/hello');
-  }
-});
-
-app.get('/card', (req, res) => {
-  res.render('card', {prompt:"Who is buried in Grant's tomb?", hint: "Think about whose tomb it is."});
-});
-
-app.get('/hello', (req, res) => {
-  const name = req.cookies.username;
-  if (name) {
-    res.redirect('/');
-  } else {
-    res.render('hello');
-  }
-});
-
-app.post('/hello', (req, res) => {
-  res.cookie('username', req.body.username);
-  res.redirect('/');
-});
-
-app.post('/goodbye', (req, res) => {
-  res.clearCookie('username');
-  res.redirect('/hello');
-});
+// app.use((req, res, next) => {
+//   console.log("world");
+//   next();
+// });
 
 app.use((req, res, next) => {
   const err = new Error("Not Found, Bitches");
